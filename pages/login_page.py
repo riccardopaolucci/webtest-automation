49376246a50a28088105e408fff0666854390a00
Riccardo Paolucci <zlac178@live.rhul.ac.uk>
2025-10-30 13:02:28 +0000

    fix(tests): stabilize Wikipedia & login tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver):
@@ -7,6 +9,7 @@ class LoginPage:
         self.password = (By.ID, "password")
         self.login_btn = (By.CSS_SELECTOR, "button[type='submit']")
         self.flash = (By.ID, "flash")
+        self.wait = WebDriverWait(self.driver, 10)
 
     def login(self, user, pwd):
         self.driver.find_element(*self.username).send_keys(user)
@@ -14,4 +17,6 @@ class LoginPage:
         self.driver.find_element(*self.login_btn).click()
 
     def get_message(self):
-        return self.driver.find_element(*self.flash).text
+        # wait for the flash message to appear and return its text
+        flash_el = self.wait.until(EC.visibility_of_element_located(self.flash))
+        return flash_el.text
